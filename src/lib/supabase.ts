a9e30313cdc5fe0b3ceabfc0bb6d92ed5e476e29
Ten2005 <stuzeukni0901@icloud.com>
2025-05-06 11:18:05 +0900

    change env vari

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabaseUrl = 'https://evqefyrjdvjktcisxqww.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2cWVmeXJqZHZqa3RjaXN4cXd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0NjUzNTQsImV4cCI6MjA1MzA0MTM1NH0.K2iviAVHFTUGJLqNzYQ7MPjQ5dEG3mByqAFJroxbQq8'
+const supabaseUrl = 'https://fjuiocwtweuxjmthlwev.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqdWlvY3d0d2V1eGptdGhsd2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MzM3MTEsImV4cCI6MjA2MTQwOTcxMX0.xPoCPRDctNqt7wAwacCNtlx3GH96vbUSk1fa-tMiYHo'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
