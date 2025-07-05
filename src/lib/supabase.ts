3107d69d0155037c75471c9c2e59e5b5e7ca4cf9
Ten Suzuki <122023835+Ten2005@users.noreply.github.com>
2025-07-06 08:29:01 +0900

    Delete src/lib/supabase.ts

--- src/lib/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://fjuiocwtweuxjmthlwev.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqdWlvY3d0d2V1eGptdGhsd2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MzM3MTEsImV4cCI6MjA2MTQwOTcxMX0.xPoCPRDctNqt7wAwacCNtlx3GH96vbUSk1fa-tMiYHo'
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
